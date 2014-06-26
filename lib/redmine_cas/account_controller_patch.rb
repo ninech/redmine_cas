71149d96badbf4e6a393d7e55ee3bb2db26161e0
Johan Vervloet <johan.vervloet@gmail.com>
2014-06-26 15:06:00 +0200

    refs #9 - CAS-login for redmine with public content.
    With this hack, you can have public pages and CAS-authentication.
    The only problem is that you can *only* have CAS-authentication.
    So if you configure CAS incorrectly, or you don't have an
    administrator with a CAS-account, you have to hack the database
    to get access again.
    
    I am looking for a fix.

--- lib/redmine_cas/account_controller_patch.rb
+++ lib/redmine_cas/account_controller_patch.rb
@@ -6,6 +6,7 @@ module RedmineCAS
       base.send(:include, InstanceMethods)
       base.class_eval do
         alias_method_chain :logout, :cas
+        alias_method_chain :login, :cas
       end
     end
 
@@ -15,6 +16,59 @@ module RedmineCAS
         logout_user
         CASClient::Frameworks::Rails::Filter.logout(self, home_url)
       end
+
+      def login_with_cas
+        if User.current.logged?
+          # User already logged in.
+          redirect_back_or_default my_page_path
+          return
+        end
+
+        # The rest of this file just contains what was in
+        # application_controller_patch.rb before.
+
+        if CASClient::Frameworks::Rails::Filter.filter(self)
+          user = User.find_by_login(session[:cas_user])
+
+          # Auto-create user if possible
+          if user.nil? && RedmineCAS.autocreate_users?
+            user = User.new
+            user.login = session[:cas_user]
+            user.assign_attributes(RedmineCAS.user_extra_attributes_from_session(session))
+            return cas_user_not_created(user) if !user.save
+            user.reload
+          end
+
+          return cas_user_not_found if user.nil?
+          return cas_account_pending unless user.active?
+          user.update_attribute(:last_login_on, Time.now)
+          user.update_attributes(RedmineCAS.user_extra_attributes_from_session(session))
+          if RedmineCAS.single_sign_out_enabled?
+            # logged_user= would start a new session and break single sign-out
+            User.current = user
+            start_user_session(user)
+          else
+            self.logged_user = user
+          end
+          redirect_to url_for(params.merge(:ticket => nil))
+        else
+          # CASClient called redirect_to
+        end
+      end
+
+      def cas_account_pending
+        render_403 :message => l(:notice_account_pending)
+      end
+
+      def cas_user_not_found
+        render_403 :message => l(:redmine_cas_user_not_found, :user => session[:cas_user])
+      end
+
+      def cas_user_not_created(user)
+        logger.error "Could not auto-create user: #{user.errors.full_messages.to_sentence}"
+        render_403 :message => l(:redmine_cas_user_not_created, :user => session[:cas_user])
+      end
+
     end
   end
 end
